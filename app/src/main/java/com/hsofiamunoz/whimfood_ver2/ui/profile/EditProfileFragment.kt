9f05d3b5b9f08092b23692763623be70c9836a65
hsofiamunoz <hsofia.munoz@udea.edu.co>
2021-10-07 17:33:45 -0500

    Profile notification tiene otro fragmento que del perfil nos lleva a editar los datos

---
+++ app/src/main/java/com/hsofiamunoz/whimfood_ver2/ui/profile/EditProfileFragment.kt
@@ -0,0 +1,37 @@
+package com.hsofiamunoz.whimfood_ver2.ui.profile
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.hsofiamunoz.whimfood_ver2.R
+import com.hsofiamunoz.whimfood_ver2.databinding.FragmentEditProfileBinding
+import com.hsofiamunoz.whimfood_ver2.databinding.FragmentProfileBinding
+
+class EditProfileFragment : Fragment() {
+
+    private var _binding: FragmentEditProfileBinding? = null
+    private val binding get() = _binding!!
+
+    /*override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }*/
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentEditProfileBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+
+        return root
+    }
+
+}
